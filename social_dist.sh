6ec624bc1c7d53cc4f82d36bf82496b1bc271e12
wcranemo <66427869+wcranemo@users.noreply.github.com>
2023-01-13 22:53:12 -0500

    intermediate saving. adding shell script and associated python file to compare parameter values

---
+++ social_dist.sh
@@ -0,0 +1,60 @@
+#!/bin/zsh
+
+make
+
+rm -r *.txt #delete all old output files
+
+# sum=$(echo ".3 + 1.12" | bc -l)
+# echo $sum
+#test
+
+#params
+runs_per_param=2
+runtype="b"
+population=10000
+runtime=150
+# soc_dist_thresh=$population   #change this to a lower number to enact social distancing at some threshold
+soc_dist_thresh=2000
+connection_reduction=$(echo "50/100" | bc -l)
+
+touch out{1..$runs}.txt
+
+echo "running $runs tests"
+
+soc_dis_params=4
+# initializing array of param values
+soc_dist_arr[1]=1000
+soc_dist_arr[2]=2000
+soc_dist_arr[3]=5000
+soc_dist_arr[4]=$population
+
+
+conn_red_params=2
+conn_red_arr[1]=$(echo "80 / 100" | bc -l)
+conn_red_arr[2]=$(echo "50 / 100" | bc -l)
+
+# for i in "${soc_dist_arr[@]}"; do
+#    echo $i
+# done
+touch params.txt
+
+for k in {1..$conn_red_params}; do
+   for j in {1..$soc_dis_params}; do
+      for i in {1..$runs_per_param}; do
+
+         # echo out_${j}_${i}.txt $runtype $population $runtime ${soc_dist_arr[j]} ${connection_red_arr[k]} | ./main &
+         # echo -ne "."
+
+         echo "conn_red = ${conn_red_arr[k]}, soc_dist_thresh = ${soc_dist_arr[j]}, run # ${i}"
+         # echo "Done"
+      done
+      printf "${soc_dist_arr[j]}, ${conn_red_arr[k]} \n" >> params.txt
+      # printf "conn_red = ${conn_red_arr[k]}, soc_dist_thresh = ${soc_dist_arr[j]}" >> params.txt
+   done
+done
+wait
+# echo "sims completed"
+
+# sleep 30
+#
+# echo $runs | python mult_run_analysis.py
